bc590245df2009624bdb7adf79528203c5584180
heyjoonkim <heyjoonkim@europa.snu.ac.kr>
2025-09-15 23:00:31 +0900

    update code

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - 어차피 huggingface 쓸꺼면 얘네 프레임워크 맞춰서 학습하는게 편함.
 - <code>dataset/preprocess.ipynb</code> 에서 그냥 하나씩 실행시키면서 최종 파일 저장
     - 우리가 만든 json 파일에서 huggingface에서 쓸 수 있는 데이터셋 형태로 변환하는 코드임.
+
+# 학습하기
+- build_dataset 돌리고, - train 돌리면 됨
--- training/models/__init__.py
+++ training/models/__init__.py
@@ -1,4 +1,1 @@
 from .load_model import load_transformers_model_and_tokenizer, load_vllm_model_and_tokenizer, get_vllm_param
-# from .openai_client_old import OpenAIClient
-from .openai_client import OpenAIClient
-from .openai_client_async import AsyncOpenAIClient
